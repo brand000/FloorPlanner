6b6a4c7ca7b2b948eab1738d5457cf2d69294bfd
brand000 <branwatson7777@gmail.com>
2021-03-10 00:27:36 -0400

    added rectangle outline to plan and window to elevation

--- scripts/projectXS.js
+++ scripts/projectXS.js
@@ -1,20 +1,77 @@
 // projectXS.js
 // LINKED to index.html
 
+const SCL = 1.5;
+
 function setup(){
     const logo_Obj = document.getElementById("logo");
     const logo = logo_Obj.getContext("2d");
-    const planViewObj = document.getElementById("plan-view");
-    const elevationViewObj = document.getElementById("elevation-view");
-
-    const planViewCon = planViewObj.getContext("2d");
-    const elevationViewCon = elevationViewObj.getContext("2d");
     
-    fillCanvas(planViewObj, planViewCon, "#D2CBCD");
-    fillCanvas(elevationViewObj, elevationViewCon, "#A3BCFD");
+    const plan = document.getElementById("plan-view");
+    const elevation = document.getElementById("elevation-view");
+
+    const planViewContext = plan.getContext("2d");
+    const elevationViewContext = elevation.getContext("2d");
+    
+    fillCanvas(plan, planViewContext, "#D2CBCD");
+    fillCanvas(elevation, elevationViewContext, "#A3BCFD");
 
     addLogo(logo);
 
+    planViewContext.clearRect(0, 0, plan.width, plan.height);
+    elevationViewContext.clearRect(0, 0, elevation.width, elevation.height);
+
+    // slab
+    planViewContext.fillStyle = "#d2cbcd"; // concrete porch
+    planViewContext.fillRect(0, 0, plan.width, plan.height);
+    // outer skin
+    planViewContext.fillStyle = "#3104fb"; // blue
+    planViewContext.fillRect(0, 0, plan.width, plan.height);
+    // interior of wall
+    planViewContext.fillStyle = "#d2cbcd"; // concrete
+    planViewContext.fillRect(
+        1 * SCL,
+        1 * SCL,
+        plan.width - 2 * SCL,
+        plan.height - 2 * SCL
+    );
+    // inner skin
+    planViewContext.fillStyle = "#3104fb"; // blue
+    planViewContext.fillRect(
+        2 * SCL,
+        2 * SCL,
+        plan.width - 4 * SCL,
+        plan.height - 4 * SCL
+    );
+    // interior floor
+    planViewContext.fillStyle = "#d2cbcd"; // concrete
+    planViewContext.fillRect(
+        3 * SCL,
+        3 * SCL,
+        plan.width - 6 * SCL,
+        plan.height - 6 * SCL
+    );
+
+    // elevation wall
+    elevationViewContext.fillStyle = "#a3bcfd"; // light blue to give hint
+    elevationViewContext.fillRect(0, 0, elevation.width, elevation.height);
+
+    // set initial slider value 2" output for display purposes only
+    $("#OpaqueInput").val(2);
+
+    // set initial slider value 0" output for display purposes only
+    $("#windowAreaInput").val(0);
+
+    // register the wall thickness slider
+    $("#OpaqueSld").on("change", function () {
+        processInput();
+    });
+
+    // register the window thickness slider
+    $("#windowAreaSld").on("change", function () {
+        processInput();
+    });
+        
 }
 
 function fillCanvas(obj, context, color){
@@ -31,4 +88,127 @@ function addLogo(context){
     
     context.font = "60px Georgia";
     context.strokeText("Xs",210, 50);
+}
+
+function processInput() {
+    let construction = $("#WindowAreaSld").val();
+    draw(construction);
+}
+
+function draw(construction, window) {
+    const plan = document.getElementById("plan-view");
+    const elevation = document.getElementById("elevation-view");
+
+    const planViewContext = plan.getContext("2d");
+    const elevationViewContext = elevation.getContext("2d");
+  
+    planViewContext.clearRect(0, 0, plan.width, plan.height);
+    elevationViewContext.clearRect(0, 0, elevation.width, elevation.height);
+  
+    // update slider value outputs for display purposes only
+    $("#OpaqueInput").val(construction);
+    $("#windowAreaInput").val(window * 2);
+  
+    // PLAN *********************************************************************
+    // slab
+    planViewContext.fillStyle = "#d2cbcd"; // concrete porch
+    planViewContext.fillRect(0, 0, plan.width, plan.height);
+    // outer skin
+    planViewContext.fillStyle = "#3104fb"; // blue
+    planViewContext.fillRect(0, 0, plan.width, plan.height);
+    // interior of wall
+    planViewContext.fillStyle = "#d2cbcd"; // concrete
+    planViewContext.fillRect(1, 1, plan.width - 2, plan.height - 2);
+    // inner skin
+    planViewContext.fillStyle = "#3104fb";
+    planViewContext.fillRect(
+      construction * SCL + Number(2),
+      construction * SCL + Number(2),
+      plan.width - 2 * construction * SCL - 4,
+      plan.height - 2 * construction * SCL - 4
+    );
+    // interior floor
+    planViewContext.fillStyle = "#d2cbcd"; // concrete
+    planViewContext.fillRect(
+      construction * SCL + Number(3),
+      construction * SCL + Number(3),
+      plan.width - 2 * construction * SCL - 6,
+      plan.height - 2 * construction * SCL - 6
+    );
+  
+    // plan wipe with concrete
+    planViewContext.fillStyle = "#d2cbcd"; // concrete
+    planViewContext.fillRect(
+      100 * SCL - window * SCL,
+      plan.height - construction * SCL - 2 * SCL,
+      2 * window * SCL,
+      construction * SCL + Number(2 * SCL)
+    );
+    // plan window
+    planViewContext.fillStyle = "#07ebf8"; // glass
+    planViewContext.fillRect(
+      100 * SCL - window * SCL,
+      plan.height - construction * SCL - 2 * SCL,
+      2 * window * SCL,
+      construction * SCL + Number(2 * SCL)
+    );
+  
+    // plan window inner threshold
+    planViewContext.setLineDash([4, 3]);
+    planViewContext.beginPath();
+    planViewContext.moveTo(
+      100 * SCL - window * SCL,
+      plan.height - construction * SCL - 2 * SCL
+    );
+    planViewContext.lineTo(
+      100 * SCL + Number(window * SCL),
+      plan.height - construction * SCL - 2 * SCL
+    );
+    planViewContext.stroke();
+    // plan window outer threshold
+    planViewContext.beginPath();
+    planViewContext.moveTo(100 * SCL - window * SCL, plan.height);
+    planViewContext.lineTo(100 * SCL + Number(window * SCL), plan.height);
+    planViewContext.stroke();
+  
+    // ELEVATION ****************************************************************
+    // elevation wall
+    elevationViewContext.fillStyle = "#a3bcfd"; // light blue to give hint
+    elevationViewContext.fillRect(0, 0, elevation.width, elevation.height);
+  
+    // elevation window 4 x 3 aspect ratio
+    // elevation window frame
+    // black
+    elevationViewContext.fillStyle = "black";
+    elevationViewContext.fillRect(
+      100 * SCL - window * SCL,
+      25 * SCL,
+      2 * window * SCL + Number(6),
+      Number(((3 * window) / 2) * SCL) + Number(4)
+    );
+    // blue
+    elevationViewContext.fillStyle = "#a3bcfd";
+    elevationViewContext.fillRect(
+      101 * SCL - window * SCL,
+      26 * SCL,
+      2 * window * SCL + Number(3),
+      Number(((3 * window) / 2) * SCL) + Number(1)
+    );
+    // elevation window
+    // black
+    elevationViewContext.fillStyle = "black";
+    elevationViewContext.fillRect(
+      102 * SCL - window * SCL,
+      27 * SCL,
+      2 * window * SCL,
+      Number(((3 * window) / 2) * SCL) - 2
+    );
+    // blue
+    elevationViewContext.fillStyle = "#a3bcfd";
+    elevationViewContext.fillRect(
+      103 * SCL - window * SCL,
+      28 * SCL,
+      2 * window * SCL - 2,
+      Number(((3 * window) / 2) * SCL) - 4
+    );
 }
